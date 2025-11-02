72ebbde1328652e138368930a366d06a15e5c2f1
Jose Rodriguez <e.jrodriguez98@go.ugr.es>
2025-11-02 10:14:45 +0100

    Implement RoPE

--- src/modules/__init__.py
+++ src/modules/__init__.py
@@ -2,5 +2,6 @@ from .embedding import Embedding
 from .ffn import FFN
 from .linear import Linear
 from .norm import RMSNorm
+from .rope import RotaryPositionEmbedding
 
-__all__ = ["Linear", "Embedding", "RMSNorm", "FFN"]
+__all__ = ["Linear", "Embedding", "RMSNorm", "FFN", "RotaryPositionEmbedding"]
